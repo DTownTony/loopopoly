7234df91c66913cd2daf9f52ba7f10cc33f6553a
nikthenak <nick@niknak.games>
2025-09-20 08:38:16 -0700

    Refactor event UI to use generic base class
    Introduced EventUIBase<T> and EventUIArgsBase to encapsulate shared UI logic for event UIs. ChoiceEventUI now inherits from EventUIBase, reducing code duplication and improving maintainability.

--- Assets/_Game/UI/ChoiceEventUI.cs
+++ Assets/_Game/UI/ChoiceEventUI.cs
@@ -3,36 +3,23 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ChoiceEventUI : MonoBehaviour
+public class ChoiceEventUI : EventUIBase<ChoiceEventUIArgs>
 { 
-    [SerializeField] private EventView _eventView;
-
-    [SerializeField] private TMP_Text _headerText;
-    [SerializeField] private Image _icon;
-    [SerializeField] private TMP_Text _descriptionText;
-    
     [SerializeField] private Button _positiveButton;
     [SerializeField] private Button _negativeButton;
     
     [SerializeField] private TMP_Text _positiveText;
     [SerializeField] private TMP_Text _negativeText;
     
-    private ChoiceEventUIArgs _currentArgs;
-
     private void Awake()
     {
         _positiveButton.onClick.AddListener(PositiveButtonPressed);
         _negativeButton.onClick.AddListener(NegativeButtonPressed);
     }
 
-    public void Show(ChoiceEventUIArgs args)
+    protected override void Show(ChoiceEventUIArgs args)
     {
-        gameObject.SetActive(true);
-        _currentArgs = args;
-        
-        _headerText.SetText(_currentArgs.Header);
-        _icon.sprite = _currentArgs.Icon;
-        _descriptionText.SetText(_currentArgs.Description);
+        base.Show(args);
         
         _positiveText.SetText(_currentArgs.PositiveButtonString);
         _negativeText.SetText(_currentArgs.NegativeButtonString);
@@ -51,36 +38,22 @@ public class ChoiceEventUI : MonoBehaviour
         _currentArgs.NegativeButtonEvent?.Invoke();
         Hide();
     }
-
-    private void Hide()
-    {
-        gameObject.SetActive(false);
-        _eventView.Hide();
-        GameController.Instance.ChangeCurrentState(GameState.WaitingForPlayer);
-    }
 }
 
-public class ChoiceEventUIArgs
+public class ChoiceEventUIArgs : EventUIArgsBase
 {
-    public string Header;
-    public Sprite Icon;
-    public string Description;
+    public readonly string PositiveButtonString;
+    public readonly string NegativeButtonString;
     
-    public string PositiveButtonString;
-    public string NegativeButtonString;
-    
-    public Action PositiveButtonEvent;
-    public Action NegativeButtonEvent;
+    public readonly Action PositiveButtonEvent;
+    public readonly Action NegativeButtonEvent;
 
-    public bool PositiveButtonEnabled;
+    public readonly bool PositiveButtonEnabled;
 
     public ChoiceEventUIArgs(string header, Sprite icon, string description, 
         Action positiveButtonEvent, Action negativeButtonEvent, string positiveButtonString, string negativeButtonString,
-        bool positiveButtonEnabled = true)
+        bool positiveButtonEnabled = true) : base(header, icon, description)
     {
-        Header = header;
-        Icon = icon;
-        Description = description;
         PositiveButtonEvent = positiveButtonEvent;
         NegativeButtonEvent = negativeButtonEvent;
         PositiveButtonString = positiveButtonString;
---
+++ Assets/_Game/UI/EventUIBase.cs
@@ -0,0 +1,45 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class EventUIBase<T> : MonoBehaviour where T : EventUIArgsBase
+{
+    [SerializeField] private EventView _eventView;
+    
+    [SerializeField] private TMP_Text _headerText;
+    [SerializeField] private Image _icon;
+    [SerializeField] private TMP_Text _descriptionText;
+
+    protected T _currentArgs;
+
+    protected virtual void Show(T args)
+    {
+        _currentArgs = args;
+        gameObject.SetActive(true);
+        
+        _headerText.SetText(_currentArgs.Header);
+        _icon.sprite = _currentArgs.Icon;
+        _descriptionText.SetText(_currentArgs.Description);
+    }
+    
+    protected void Hide()
+    {
+        gameObject.SetActive(false);
+        _eventView.Hide();
+        GameController.Instance.ChangeCurrentState(GameState.WaitingForPlayer);
+    }
+}
+
+public class EventUIArgsBase
+{
+    public string Header;
+    public Sprite Icon;
+    public string Description;
+
+    public EventUIArgsBase(string header, Sprite icon, string description)
+    {
+        Header = header;
+        Icon = icon;
+        Description = description;
+    }
+}
