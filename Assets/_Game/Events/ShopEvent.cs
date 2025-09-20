3a0013950e5add4257dc138b6a432131144c1a57
nikthenak <nick@niknak.games>
2025-09-20 09:03:53 -0700

    shop fix

---
+++ Assets/_Game/Events/ShopEvent.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ShopEvent", menuName = "Data/BoardEvent/ShopEvent")]
+public class ShopEvent : BoardEvent
+{
+    [SerializeField] private ItemDatabase _itemDatabase;
+    
+    private const int TOTAL_ITEMS = 3;
+    
+    public override void Trigger()
+    {
+        var items = new List<ItemData>();
+        for (var i = 0; i < TOTAL_ITEMS; i++)
+        {
+            var randomItem = _itemDatabase.GetRandomItem();
+            items.Add(randomItem);
+        }
+        
+        GameController.Instance.EventHandler.ShowItemsEvent(new ItemsEventUIArgs(
+            Name, null, null, items));
+    }
+}
